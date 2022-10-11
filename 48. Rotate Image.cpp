adb923dceb43cd6690cf6b8d8fc34d3e9bcb7926
Deep Sresthi <49350563+Deepsresthi@users.noreply.github.com>
2022-10-11 14:44:08 +0530

    Create 48. Rotate Image.cpp

---
+++ 48. Rotate Image.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        for(int i=0; i<matrix.size(); i++){
+            for(int j=i; j<matrix.size();j++){
+                int temp=0;
+                temp = matrix[i][j];
+                matrix[i][j]= matrix[j][i];
+                matrix[j][i]= temp;
+            }
+        }
+        for(int i=0; i<matrix.size(); i++){
+            for(int j=0; j<matrix.size()/2; j++){
+                int temp=0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.size()-1-j];
+                matrix[i][matrix.size()-1-j] = temp;
+            }
+        }
+    }
+};
