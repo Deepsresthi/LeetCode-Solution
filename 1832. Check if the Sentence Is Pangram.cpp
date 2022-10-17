e2e8bf1870f4a7df5ae6155b77cb59b1c6ca2f4b
Deep Sresthi <49350563+Deepsresthi@users.noreply.github.com>
2022-10-17 17:25:29 +0530

    Create 1832. Check if the Sentence Is Pangram.cpp

---
+++ 1832. Check if the Sentence Is Pangram.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        unordered_map<char, int> mp;
+        for(int i=0; i<sentence.size(); i++){
+            mp[sentence[i]]++;
+        }
+        if(mp.size()==26) return true;
+        else return false;
+    }
+};
